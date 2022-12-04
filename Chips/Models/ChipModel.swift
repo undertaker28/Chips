df3bb3fccbb30f81ce553057e8edfb62ac6ab181
undertaker28 <pavel.starovoytovvv@gmail.com>
2022-12-04 18:08:15 +0300

    Add models

---
+++ Chips/Models/ChipModel.swift
@@ -0,0 +1,14 @@
+//
+//  ChipModel.swift
+//  Chips
+//
+//  Created by Pavel on 4.12.22.
+//
+
+import SwiftUI
+
+struct ChipModel: Identifiable {
+    let id = UUID()
+    @State var isSelected: Bool
+    let titleKey: String
+}
