7bfa319314675e32db4f7950cf42f06d35a55f90
undertaker28 <pavel.starovoytovvv@gmail.com>
2022-12-04 16:50:27 +0300

    Initial Commit

---
+++ Chips/ChipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChipsApp.swift
+//  Chips
+//
+//  Created by Pavel on 4.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ChipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
